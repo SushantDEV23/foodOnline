cbe7813ea686834636c28abd46a1c78df9519d12
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-05-28 00:01:39 +0530

    Custom User, Media Files, User profile

---
+++ foodOnline/accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import User, UserProfile
+from django.contrib.auth.admin import UserAdmin
+
+#We wrote the below code so that user's password should not be editable in admin panel
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name', 'role', 'is_staff')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
