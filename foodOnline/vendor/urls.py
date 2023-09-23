b13b012cc16c7b1783170d7e666c52226b3412ae
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-09-23 22:14:22 +0530

    menu category food links

--- foodOnline/vendor/urls.py
+++ foodOnline/vendor/urls.py
@@ -5,5 +5,6 @@ from accounts import views as AccountViews
 urlpatterns = [
     path('', AccountViews.vendorDashboard, name='vendor'),
     path('profile/', vprofile, name='vprofile'),
-    path('menu-builder/', menu_builder, name='menu_builder')
+    path('menu-builder/', menu_builder, name='menu_builder'),
+    path('menu-builder/category/<int:pk>/', fooditems_by_category, name='fooditems_by_category')
 ]
--- foodOnline/vendor/views.py
+++ foodOnline/vendor/views.py
@@ -10,6 +10,10 @@ from django.core.exceptions import PermissionDenied
 from accounts.views import check_role_vendor
 from menu.models import *
 
+def get_vendor(request):                      # Call this function when getting vendor object
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
+
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
 def vprofile(request):
@@ -38,12 +42,25 @@ def vprofile(request):
     }
     return render(request, 'vendor/vprofile.html', context)
 
-
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def menu_builder(request):
-    vendor = Vendor.objects.get(user=request.user)
+    vendor = get_vendor(request)
     categories = Category.objects.filter(vendor=vendor)
     context = {
         'categories':categories
     }
     return render(request, 'vendor/menu_builder.html', context)
+
+@login_required(login_url='login')
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        'fooditems':fooditems,
+        'category':category
+    }
+    return render(request, 'vendor/fooditems_by_category.html', context)
 
