ed328c374f5bb0b6d8c4935d25489caaa6612dc7
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-10-02 20:07:42 +0530

    edit+delete operation on category

--- foodOnline/vendor/urls.py
+++ foodOnline/vendor/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('menu-builder/category/<int:pk>/', fooditems_by_category, name='fooditems_by_category'),
 
     #Category CRUD
-    path('menu-builder/category/add/', add_category, name='add_category')
+    path('menu-builder/category/add/', add_category, name='add_category'),
+    path('menu-builder/category/edit/<int:pk>/', edit_category, name='edit_category'),
+    path('menu-builder/category/delete/<int:pk>/', delete_category, name='delete_category')
 ]
