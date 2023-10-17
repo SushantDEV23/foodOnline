6f9d7b67845b89687031513111a7b1802bf409c7
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-10-17 21:04:59 +0530

    Delete food CRUD

--- foodOnline/vendor/views.py
+++ foodOnline/vendor/views.py
@@ -142,6 +142,7 @@ def add_food(request):
             print(form.errors)
     else:
         form = FoodItemForm()
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form':form,
     }
@@ -164,6 +165,7 @@ def edit_food(request, pk=None):
             print(form.errors)
     else:
         form = FoodItemForm(instance=food)
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form':form,
         'food':food
