a78830a8b5ce066eafa90c8e4406340282695f5e
Sushant Nerkar <sushantnerkarjbp@gmail.com>
2023-10-22 20:12:25 +0530

    changes made to home.html

--- foodOnline/foodOnline/urls.py
+++ foodOnline/foodOnline/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', home, name='home')
+    path('home/', home, name='home')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
